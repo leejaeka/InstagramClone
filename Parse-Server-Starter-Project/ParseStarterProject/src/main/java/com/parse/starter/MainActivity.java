dd933cf2523b2e3df784afeafc9590925ac4dbc1
Jaekang Lee <jaekang.lee@mail.utoronto.ca>
2020-06-17 19:39:55 -0700

    parse practice might delete later

--- Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ Parse-Server-Starter-Project/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -14,11 +14,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
 
+import com.parse.GetCallback;
 import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
@@ -31,8 +34,52 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    
-    ParseAnalytics.trackAppOpenedInBackground(getIntent());
-  }
+//    ParseObject score = new ParseObject("Score");
+//    score.put("username", "rob");
+//    score.put("score", 87);
+//
+//    score.saveInBackground(new SaveCallback() {
+//      @Override
+//      public void done(ParseException e) {
+//        if (e == null){
+//          Log.i("SaveInBackground", "Succesful");
+//        }else{
+//          Log.i("SaveInBackground", "Failed: Error: "+e.toString());
+//        }
+//      }
+//    });
+    //GET FROM PARSE
+//    ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+//    query.getInBackground("SFsgzJ1a0E", new GetCallback<ParseObject>() {
+//      @Override
+//      public void done(ParseObject object, ParseException e) {
+//        if (e== null && object != null){
+//          object.put("score",200);
+//          object.saveInBackground();
+//          Log.i("ObjectValue", object.getString("username"));
+//          Log.i("ObjectValue", Integer.toString(object.getInt("score")));
+//          //UPDATE
+//
+//
+//
+//        }else{
+//          Log.i("SaveInBackground", "Failed: Error: "+e.toString());
+//        }
+//      }
+//    });workddl2737  Dlworkd2737
+//    ParseObject tweet = new ParseObject("Tweet");
+//    tweet.put("username","tommy");
+//    tweet.saveInBackground(new SaveCallback() {
+//      @Override
+//      public void done(ParseException e) {
+//        if (e==null){
+//          Log.i("Tweet","success");
+//        } else{
+//          Log.i("Tweet", "Error: "+e.toString());
+//        }
+//      }
+//    });
+//    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+//  }
 
 }
